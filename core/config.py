b9b62daba4bfa0df0c5f2e222cfb8ed5568445c0
byandrev <byandrev@gmail.com>
2023-01-25 19:58:56 -0500

    add cors

--- core/config.py
+++ core/config.py
@@ -8,10 +8,10 @@ load_dotenv(dotenv_path=env_path)
 class Settings:
     PROJECT_NAME: str = "Lnks"
     PROJECT_VERSION: str = "1.0.0"
-    
+
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     DATABASE_NAME: str = os.getenv("DATABASE_NAME")
-    
+
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
+from core.cors import origins
 from routers.base import router
 
 
 def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    app = FastAPI(title=settings.PROJECT_NAME,
+                  version=settings.PROJECT_VERSION)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
     app.include_router(router)
     return app
 
@@ -25,5 +37,5 @@ def shutdown_db_client():
     pass
 
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
